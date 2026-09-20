9d5b3387af48ef3142a81a7c61f24cf829147718
prathi6066 <62275666+prathi6066@users.noreply.github.com>
2026-09-20 14:16:53 -0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+
+        var left = 0
+        var right = nums.size - 1
+
+        while(left <= right) {
+            val mid: Int = left + (right - left) / 2
+            if (nums[mid] < target) {
+                left = mid + 1
+            } else if (nums[mid] > target) {
+                right = mid - 1
+            } else {
+                return mid
+            }
+
+        }
+
+        return -1
+
+    }
+}
